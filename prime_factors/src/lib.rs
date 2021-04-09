7f137bb8adac027591b32bbf0b6687e04c2ea95c
Oliver Forral <intrepion@gmail.com>
2021-04-08 20:29:52 -0700

    bring function into scope of tests

--- prime_factors/src/lib.rs
+++ prime_factors/src/lib.rs
@@ -3,6 +3,7 @@ pub fn prime_factors() {}
 #[cfg(test)]
 mod tests {
         #[test]
+        use crate::prime_factors;
         fn prime_factors_of_two() {
                 assert_eq!(prime_factors(2), [2]);
         }
