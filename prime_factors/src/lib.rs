d17750992add16a60a36ed4354f53cf4e97643fe
Oliver Forral <intrepion@gmail.com>
2021-04-08 20:56:32 -0700

    make test pass

--- prime_factors/src/lib.rs
+++ prime_factors/src/lib.rs
@@ -1,9 +1,11 @@
-pub fn prime_factors(num: i32) -> Vec<i32> {}
+pub fn prime_factors(num: i32) -> Vec<i32> {
+        vec![2]
+}
 
 #[cfg(test)]
 mod tests {
-        #[test]
         use crate::prime_factors;
+        #[test]
         fn prime_factors_of_two() {
                 assert_eq!(prime_factors(2), [2]);
         }
