4fe2352a6753cd0752752d5653de3a7e133768b4
Oliver Forral <intrepion@gmail.com>
2021-04-08 21:18:38 -0700

    refactor

--- prime_factors/src/lib.rs
+++ prime_factors/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn prime_factors(mut num: i32) -> Vec<i32> {
         let mut answer = vec![];
-        while num != 1 {
-                for candidate in 2..=num {
-                        if num % candidate == 0 {
-                                answer.push(candidate);
-                                num = num / candidate;
-                                break;
-                        }
+        let mut candidate = 2;
+
+        while num > 1 {
+                while num % candidate == 0 {
+                        answer.push(candidate);
+                        num /= candidate;
                 }
+                candidate += 1;
         }
         answer
 }
