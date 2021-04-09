93b6da030ab62361757f8b4d8380f36f96fffd5e
Oliver Forral <intrepion@gmail.com>
2021-04-08 20:59:52 -0700

    Added test for factors of 3

--- prime_factors/src/lib.rs
+++ prime_factors/src/lib.rs
@@ -5,8 +5,14 @@ pub fn prime_factors(num: i32) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
         use crate::prime_factors;
+
         #[test]
         fn prime_factors_of_two() {
                 assert_eq!(prime_factors(2), [2]);
+        }
+
+        #[test]
+        fn prime_factors_of_three() {
+                assert_eq!(prime_factors(3), [3]);
         }
 }
